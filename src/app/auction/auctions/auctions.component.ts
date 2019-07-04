d237ad974b2d04f8d89bffcbac2eb152a2c4b3d2
MaciejLisCK <accounts@maciejlis.com>
2019-07-04 16:50:48 +0200

    lista statycznych aukcji

--- src/app/auction/auctions/auctions.component.ts
+++ src/app/auction/auctions/auctions.component.ts
@@ -6,7 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./auctions.component.css']
 })
 export class AuctionsComponent implements OnInit {
-
+  auctions: AuctionsItemp[] = [{
+    title: 'Domek',
+    description: 'jakiś tam opis2',
+    imgUrl: 'https://picsum.photos/id/236/200/200',
+    price: 10
+  }];
   constructor() { }
 
   ngOnInit() {
