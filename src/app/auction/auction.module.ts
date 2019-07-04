de83291654a025d8e94936ef07ee8aa39226834b
MaciejLisCK <accounts@maciejlis.com>
2019-07-04 16:45:49 +0200

    dodana karta aukukcji 2

--- src/app/auction/auction.module.ts
+++ src/app/auction/auction.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuctionsComponent } from './auctions/auctions.component';
 import { PromotionsComponent } from './promotions/promotions.component';
+import { AuctionItemComponent } from './auctions/auction-item/auction-item.component';
 
 
 
 @NgModule({
   declarations: [
     AuctionsComponent,
-    PromotionsComponent
+    PromotionsComponent,
+    AuctionItemComponent
   ],
   imports: [
     CommonModule
