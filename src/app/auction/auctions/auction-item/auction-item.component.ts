9266d74892402ef3a1cce281ac288cbed6e3f99a
MaciejLisCK <accounts@maciejlis.com>
2019-07-04 16:49:22 +0200

    dodana karta aukukcji 3

---
+++ src/app/auction/auctions/auction-item/auction-item.component.ts
@@ -0,0 +1,30 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { AuctionItem } from '../../auction-item';
+
+@Component({
+  selector: 'ap-auction-item',
+  template: `
+  <div class="card">
+    <div class="card-header">{{auction.title}}</div>
+    <img class="card-img" [src]="auction.imgUrl">
+    <div class="card-body">
+        <p class="card-text">
+            {{auction.description}}
+        </p>
+        <button class="btn btn-primary">
+            <i class="fa fa-cart-plus"></i>
+        </button>
+    </div>
+  </div>
+  `,
+  styles: []
+})
+export class AuctionItemComponent implements OnInit {
+  @Input() auction: AuctionItem;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
