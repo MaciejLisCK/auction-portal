05f04f3161c661275f152be3e06d428bca591b97
MaciejLisCK <accounts@maciejlis.com>
2019-07-04 18:36:27 +0200

    pipeline

--- src/app/auction/promotions/promotions.component.ts
+++ src/app/auction/promotions/promotions.component.ts
@@ -4,11 +4,15 @@ import { Component, OnInit } from '@angular/core';
   selector: 'ap-promotions',
   template: `
     <h2> Promocje: </h2>
+    <section>
+      <p> {{ today | date:'dd.MM.yyyy' }} </p>
+      <p> {{ 'Hello World' | uppercase | lowercase }} </p>
+    </section>
   `,
   styles: []
 })
 export class PromotionsComponent implements OnInit {
-
+  today = new Date();
   constructor() { }
 
   ngOnInit() {
