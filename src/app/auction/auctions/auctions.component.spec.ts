0a559c81698a78e20448eaac8b3d866962716ca2
MaciejLisCK <accounts@maciejlis.com>
2019-07-05 19:32:49 +0200

    test - inna metoda (stub)

--- src/app/auction/auctions/auctions.component.spec.ts
+++ src/app/auction/auctions/auctions.component.spec.ts
@@ -5,16 +5,29 @@ import {HttpClientTestingModule} from '@angular/common/http/testing';
 import { AuctionsComponent } from './auctions.component';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { AuctionFilterPipe } from '../auction-filter.pipe';
+import { AuctionService } from '../auction.service';
+import { Observable, of } from 'rxjs';
+import { AuctionItem } from '../auction-item';
+
 
 fdescribe('AuctionsComponent', () => {
   let component: AuctionsComponent;
   let fixture: ComponentFixture<AuctionsComponent>;
 
+  class AuctionServiceStub {
+    getAll(): Observable<AuctionItem[]> {
+      return of([]);
+    }
+  }
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ AuctionsComponent, AuctionFilterPipe ],
+      providers: [
+        {provide: AuctionService, useClass: AuctionServiceStub}
+      ],
       imports: [
-        HttpClientTestingModule
+        // HttpClientTestingModule
       ],
       schemas: [NO_ERRORS_SCHEMA]
     })
