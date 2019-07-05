3c56cbbf5d7cc9fba4747ecc9d12f4d74a8bdb28
MaciejLisCK <accounts@maciejlis.com>
2019-07-05 19:08:39 +0200

    test- naprawa testu

--- src/app/main-menu/main-menu.component.spec.ts
+++ src/app/main-menu/main-menu.component.spec.ts
@@ -1,14 +1,16 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { MainMenuComponent } from './main-menu.component';
+import { RouterTestingModule } from '@angular/router/testing';
 
-describe('MainMenuComponent', () => {
+fdescribe('MainMenuComponent', () => {
   let component: MainMenuComponent;
   let fixture: ComponentFixture<MainMenuComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ MainMenuComponent ]
+      declarations: [ MainMenuComponent ],
+      imports: [RouterTestingModule]
     })
     .compileComponents();
   }));
