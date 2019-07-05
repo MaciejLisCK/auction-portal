458a3cdd06e75740ccb07475661bf91241e658e2
MaciejLisCK <accounts@maciejlis.com>
2019-07-05 17:03:43 +0200

    Odsubskrybowanie się od strumienia przy wolnym internecie

--- src/app/auction/auctions/auctions.component.ts
+++ src/app/auction/auctions/auctions.component.ts
@@ -1,23 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import {AuctionService} from '../auction.service';
 import {AuctionItem} from './../auction-item';
 import { CartService } from '../cart.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'ap-auctions',
   templateUrl: './auctions.component.html',
   styleUrls: ['./auctions.component.css']
 })
-export class AuctionsComponent implements OnInit {
+export class AuctionsComponent implements OnInit, OnDestroy {
+
   auctions: AuctionItem[] = [];
+  auctionSub: Subscription;
+
   constructor(private auctionService: AuctionService, private cartService: CartService) { }
 
   ngOnInit() {
-    this.auctionService
+    this.auctionSub = this.auctionService
       .getAll()
       .subscribe((auctions: AuctionItem[]) => {
         this.auctions = auctions;
+        console.log(this.auctions);
       });
+  }
+
+  ngOnDestroy(): void {
+    this.auctionSub.unsubscribe();
   }
 
   handleAddToCart(auction: AuctionItem) {
