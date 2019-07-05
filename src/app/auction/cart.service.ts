e5f22608318c594ab3b4748507e30143f42eca5b
MaciejLisCK <accounts@maciejlis.com>
2019-07-05 15:16:01 +0200

    CartService mock

--- src/app/auction/cart.service.ts
+++ src/app/auction/cart.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { AuctionItem } from './auction-item';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
+  auctions: AuctionItem[] = [];
 
   constructor() { }
+
+  addToCart(item: AuctionItem) {
+    this.auctions.push(item);
+  }
+
+  getAllItems(): AuctionItem[] {
+    return this.auctions;
+  }
 }
