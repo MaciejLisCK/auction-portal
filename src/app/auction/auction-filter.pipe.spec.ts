b205a0ae946e66f83e18f8d02759f68b08965b7c
MaciejLisCK <accounts@maciejlis.com>
2019-07-05 18:54:46 +0200

    test - auction pipe

--- src/app/auction/auction-filter.pipe.spec.ts
+++ src/app/auction/auction-filter.pipe.spec.ts
@@ -1,8 +1,29 @@
 import { AuctionFilterPipe } from './auction-filter.pipe';
+import { AuctionItem } from './auction-item';
 
-describe('AuctionFilterPipe', () => {
+fdescribe('AuctionFilterPipe', () => {
   it('create an instance', () => {
     const pipe = new AuctionFilterPipe();
     expect(pipe).toBeTruthy();
+  });
+
+  it('should filter my auctions', () => {
+    // GIVEN (arrange)
+    const pipe = new AuctionFilterPipe();
+    const auctions: AuctionItem[] = [
+      {title: 'Pomidor'} as AuctionItem,
+      {title: 'Ogórek'} as AuctionItem,
+      {title: 'Parmezan'} as AuctionItem,
+    ];
+    const filterWord = 'P';
+
+    // WHEN (act)
+    const resultArray = pipe.transform(auctions, filterWord);
+
+    // THEN (assert)
+    expect(resultArray).toEqual([
+      {title: 'Pomidor'},
+      {title: 'Parmezan'}
+    ]);
   });
 });
